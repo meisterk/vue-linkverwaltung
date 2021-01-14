0ff89336fef4cb13c2cdb1e63cf538a6404c737a
Franz Kohnle <franz@kohnlehome.de>
2021-01-14 15:37:20 +0100

    deploy

---
+++ precache-manifest.21670e0562424181788a98a9181d4dc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3b9711dfe6d39a4d258",
+    "url": "/vue-linkverwaltung/css/chunk-vendors.c5b0137c.css"
+  },
+  {
+    "revision": "0cc16a6acb6670c03b4a43f0988d3617",
+    "url": "/vue-linkverwaltung/index.html"
+  },
+  {
+    "revision": "81b733fde0e72f4b58a7",
+    "url": "/vue-linkverwaltung/js/app.d646d92b.js"
+  },
+  {
+    "revision": "f3b9711dfe6d39a4d258",
+    "url": "/vue-linkverwaltung/js/chunk-vendors.8f1226ca.js"
+  },
+  {
+    "revision": "6c37e5311ef2a561d0f0a966a991f11a",
+    "url": "/vue-linkverwaltung/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-linkverwaltung/robots.txt"
+  }
+]);
